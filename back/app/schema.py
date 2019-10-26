a89dd79491e94856422c64f2965f4f435ddc48b1
Alex M <makridenko.a@yandex.ru>
2019-10-26 06:24:55 +0300

    add jwt auth

--- back/app/schema.py
+++ back/app/schema.py
@@ -10,16 +10,16 @@ class Query(
 ):
     pass
 
-'''
+
 class Mutation(
     graphene.ObjectType,
 ):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
-'''
+
 
 schema = graphene.Schema(
     query=Query,
-    #mutation=Mutation,
+    mutation=Mutation,
 )
